df02439838e66429d3933deae44077df560ca3e9
Sarah13c <sarahcb13@hotmail.com>
2024-04-20 12:43:14 -0500

    QR-Funcional

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import * as JsBarcode from 'jsbarcode';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit, OnDestroy{
+ 
+  qrString = 'Esto es un mensaje oculto';
+  barCodeString = '12345566765';
+  scannedResult: any;
+  content_visibility = '';
 
-  constructor() {}
+  constructor(
+    ) {}
 
+  ngOnInit(): void {
+    JsBarcode("#barcode", this.barCodeString, {
+      // format: "pharmacode",
+      lineColor: "#0aa",
+      width:4,
+      height:200,
+      displayValue: false
+    });
+  }
+
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
+      return false;
+    } catch(e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  async startScan() {
+    try {
+      const permission = await this.checkPermission();
+      if(!permission) {
+        return;
+      }
+      await BarcodeScanner.hideBackground();
+      
+      document.querySelector('body')?.classList.add('scanner-active');
+      this.content_visibility = 'hidden';
+      const result = await BarcodeScanner.startScan();
+      console.log(result);
+      BarcodeScanner.showBackground();
+      document.querySelector('body')?.classList.remove('scanner-active');
+      this.content_visibility = '';
+      if(result?.hasContent) {
+        this.scannedResult = result.content;
+        console.log(this.scannedResult);
+      }
+    } catch(e) {
+      console.log(e);
+      this.stopScan();
+    }
+  }
+
+  stopScan() {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body')?.classList.remove('scanner-active');
+    this.content_visibility = '';
+  }
+
+  ngOnDestroy(): void {
+      this.stopScan();
+  }
 }
